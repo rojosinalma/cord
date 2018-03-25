fddee8fcd3c8c578ef8ea0869c2d4ce78c61c160
tanguy <breakfast@cock.li>
2018-03-25 17:38:28 +0200

    Adding a GUI for managing configatron entries

---
+++ app/sinatra/gui.rb
@@ -0,0 +1,18 @@
+require 'erb'
+
+module Sinatra
+  class WebGui < Sinatra::Base
+    set :public_folder, File.dirname(__FILE__) + '/public'
+    get '/config' do
+      erb :config
+    end
+    post '/config' do
+      configatron.discord_token      = params['bot_token']
+      configatron.discord_client_id  = params['client_id']
+      configatron.discord_bot_prefix = params['bot_prefix']
+      $bot.stop
+      require_rel '../lib/discord.rb'
+      erb :config
+    end
+  end
+end
